55e9653a9694c4a0d650dc2fe06781dee5caea51
smling <smling9_9@hotmail.com>
2023-02-19 15:45:40 +0000

    add method to lookup by url.

--- src/test/java/com/github/smling/WhoIsClientTest.java
+++ src/test/java/com/github/smling/WhoIsClientTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,6 +45,28 @@ class WhoIsClientTest {
         });
     }
 
+    @ParameterizedTest
+    @MethodSource("mockLookupByUrlHappyCase")
+    void lookupByUrlHappyCase(URL value) {
+        assertDoesNotThrow(()->{
+            WhoIsClient whoIsClient = new WhoIsClient();
+            String result = whoIsClient.lookup(value);
+            assertNotNull(result, "whois lookup response:\n" +
+                    result);
+            assertTrue(result.length() > 0);
+            System.out.println("whois lookup response: \n"+result);
+        });
+    }
+
+    @ParameterizedTest
+    @MethodSource("mockLookupByUrlUnhappyCase")
+    void lookupByUrlUnhappyCase(URL value, Class<? extends Exception> exceptionClass) {
+        assertThrowsExactly(exceptionClass, ()->{
+            WhoIsClient whoIsClient = new WhoIsClient();
+            String result = whoIsClient.lookup(value);
+            System.out.println("whois lookup response: \n"+result);
+        });
+    }
     static Stream<Arguments> mockLookUpHappyCases() {
         return Stream.of(
                 Arguments.of("google.com"),
@@ -58,6 +82,23 @@ class WhoIsClientTest {
         return Stream.of(
                 Arguments.of("ap..ple", DomainNameLookupException.class),
                 Arguments.of("", DomainNameLookupException.class),
+                Arguments.of(null, DomainNameLookupException.class)
+        );
+    }
+
+    static Stream<Arguments> mockLookupByUrlHappyCase() throws MalformedURLException {
+        return Stream.of(
+                Arguments.of(new URL("http://google.com")),
+                Arguments.of(new URL("https://www.google.com")),
+                Arguments.of(new URL("https://gov.uk")),
+                Arguments.of(new URL("ftp://yahoo.com.hk"), DomainNameLookupException.class)
+        );
+    }
+
+    static Stream<Arguments> mockLookupByUrlUnhappyCase() throws MalformedURLException {
+        return Stream.of(
+                Arguments.of(new URL("http://192.168.0.1"), DomainNameLookupException.class),
+                Arguments.of(new URL("http://192.168.0.1:8964"), DomainNameLookupException.class),
                 Arguments.of(null, DomainNameLookupException.class)
         );
     }
