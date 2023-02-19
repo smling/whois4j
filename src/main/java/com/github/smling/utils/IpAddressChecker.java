fca2c57d1ce84f7e623c2d888d679182946b4aca
smling <smling9_9@hotmail.com>
2023-02-19 15:47:30 +0000

    update JavaDoc.

--- src/main/java/com/github/smling/utils/IpAddressChecker.java
+++ src/main/java/com/github/smling/utils/IpAddressChecker.java
@@ -2,7 +2,13 @@ package com.github.smling.utils;
 
 import java.util.regex.Pattern;
 
+/**
+ * Utility class to check IP address.
+ */
 public enum IpAddressChecker {
+    /**
+     * Utility instance.
+     */
     INSTANCE;
     private static final String REGULAR_EXPRESSION_0_TO_255 = "([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
     private static final Pattern IPV4_REGULAR_EXPRESSION_PATTERN = Pattern
@@ -10,6 +16,12 @@ public enum IpAddressChecker {
                     REGULAR_EXPRESSION_0_TO_255+"\\." +
                     REGULAR_EXPRESSION_0_TO_255+"\\." +
                     REGULAR_EXPRESSION_0_TO_255+"$");
+
+    /**
+     * Check value is IPv4 adress or not.
+     * @param value value to be checked.
+     * @return Boolean to indicate value is valid IPv4 or not.
+     */
     public boolean isIPv4Address(String value) {
         if(StringUtil.INSTANCE.isNullOrBlank(value)) {
             return false;
