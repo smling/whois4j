f3d13bd42fe9d18ab9ce4a66533c783e28f8c408
smling <smling9_9@hotmail.com>
2023-02-20 19:30:17 +0000

    add unit tests for exception.

---
+++ src/test/java/com/github/smling/exceptions/GeoLocationLookupExceptionTest.java
@@ -0,0 +1,21 @@
+package com.github.smling.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeoLocationLookupExceptionTest {
+    @Test
+    void canBeCreateWithMessage() {
+        assertThrowsExactly(GeoLocationLookupException.class, ()-> {
+            throw new GeoLocationLookupException("This is test exception.");
+        });
+    }
+
+    @Test
+    void canBeCreateWithMessageAndInnerException() {
+        assertThrowsExactly(GeoLocationLookupException.class, ()-> {
+            throw new GeoLocationLookupException("This is test exception", new RuntimeException());
+        });
+    }
+}
